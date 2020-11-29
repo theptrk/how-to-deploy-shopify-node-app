f1938d0706c779f91846fa32475788094f178a93
Patrick Tran <patrick.tran06@gmail.com>
2020-11-29 09:10:50 -0800

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { Heading, Page } from "@shopify/polaris";
+
+const Index = () => (
+  <Page>
+    <Heading>Welcome to our tutorial: How to deploy node app 🎉</Heading>
+  </Page>
+);
+
+export default Index;
